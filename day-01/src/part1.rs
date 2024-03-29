40720ea4b5d2f21a241fe0cf61c060be00652e2d
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-29 09:16:23 +0100

    add day01 part1

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -3,8 +3,38 @@ use crate::custom_error::AocError;
 #[tracing::instrument]
 pub fn process(
     _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+) -> miette::Result<u32, AocError> {
+    let sum = _input
+        .lines()
+        .map(|l| {
+            let mut it =
+                l.chars().filter_map(|c| c.to_digit(10));
+            let first = it.next().expect(
+                "There should be a number on each line",
+            );
+            let last = it.last().unwrap_or(first);
+
+            format!("{first}{last}").parse::<u32>().expect(
+                "First and last number should be parsable",
+            )
+        })
+        .sum();
+
+    Ok(sum)
+    // for l in _input.lines() {
+    //     let mut it = l.chars().filter(|s| s.is_numeric());
+    //     let first = it.next().unwrap();
+    //     let last = it.last().unwrap_or(first);
+    //     let cur = [first, last]
+    //         .into_iter()
+    //         .collect::<String>()
+    //         .parse::<u32>()
+    //         .unwrap();
+    //
+    //     sum += cur;
+    // }
+    //
+    // Ok(sum)
 }
 
 #[cfg(test)]
@@ -13,9 +43,12 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2 
+pqr3stu8vwx 
+a1b2c3d4e5f
+treb7uchet 
+";
+        assert_eq!(142, process(input)?);
         Ok(())
     }
 }
