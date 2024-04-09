ccd699d7f5824f883cd6df1cd43bc666e26d3ee8
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-09 22:45:43 +0200

    Add part1 day8

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -1,10 +1,85 @@
+use std::collections::HashMap;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{
+        alpha1, digit1, line_ending, multispace1, space1,
+    },
+    multi::separated_list1,
+    sequence::{
+        delimited, separated_pair, terminated, tuple,
+    },
+    IResult,
+};
+
 use crate::custom_error::AocError;
+
+fn parse_directions(line: &str) -> IResult<&str, &str> {
+    terminated(alpha1, multispace1)(line)
+}
+
+fn parse_row(
+    line: &str,
+) -> IResult<&str, (&str, (&str, &str))> {
+    tuple((
+        terminated(
+            alpha1,
+            tuple((space1, tag("="), space1, tag("("))),
+        ),
+        tuple((
+            terminated(alpha1, tuple((tag(","), space1))),
+            terminated(alpha1, tag(")")),
+        )),
+    ))(line)
+}
+
+fn parse_rows(
+    input: &str,
+) -> IResult<&str, Vec<(&str, (&str, &str))>> {
+    separated_list1(line_ending, parse_row)(input)
+}
 
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    input: &str,
+) -> miette::Result<u32, AocError> {
+    let (input, directions) =
+        parse_directions(input).unwrap();
+
+    let (_, rows) = parse_rows(input).unwrap();
+
+    let mut key = "AAA";
+
+    let h = HashMap::<&str, (&str, &str)>::from_iter(rows);
+
+    let h = directions
+        .chars()
+        .cycle()
+        .enumerate()
+        .take_while(|(i, c)| {
+            dbg!(c, key);
+            if key == "ZZZ" {
+                false
+            } else {
+                match c {
+                    'L' => {
+                        key = h.get(key).unwrap().0;
+                        true
+                    }
+                    'R' => {
+                        key = h.get(key).unwrap().1;
+                        true
+                    }
+                    _ => {
+                        panic!("Should only contain L or R")
+                    }
+                }
+            }
+        })
+        .map(|(i, _)| i + 1)
+        .last()
+        .unwrap();
+    Ok(h as u32)
 }
 
 #[cfg(test)]
@@ -12,10 +87,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+    fn test_process_1() -> miette::Result<()> {
+        let input = "\
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+        assert_eq!(2, process(input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_process_2() -> miette::Result<()> {
+        let input = "\
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+        assert_eq!(6, process(input)?);
         Ok(())
     }
 }
