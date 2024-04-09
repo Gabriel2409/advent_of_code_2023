ce22ba290e624b55714f72a8d194cc6114d8572a
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-09 21:36:03 +0200

    Add part1 day7

--- day-07/src/part1.rs
+++ day-07/src/part1.rs
@@ -1,5 +1,8 @@
 use nom::{
-    character::complete::{self, alphanumeric1, space1},
+    character::complete::{
+        self, alphanumeric1, line_ending, space1,
+    },
+    multi::separated_list1,
     sequence::{terminated, tuple},
     IResult, Parser,
 };
@@ -36,7 +39,32 @@ pub fn letter_map() -> &'static HashMap<char, u32> {
 struct Hand {
     cards: Vec<u32>,
     strength: u32,
+    tie_break: u32,
     bid: u32,
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // First compare by strength
+        let strength_ordering =
+            self.strength.cmp(&other.strength);
+
+        // If strengths are equal, compare by tie_break
+        if strength_ordering == std::cmp::Ordering::Equal {
+            self.tie_break.cmp(&other.tie_break)
+        } else {
+            strength_ordering
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(
+        &self,
+        other: &Self,
+    ) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Hand {
@@ -50,32 +78,36 @@ impl Hand {
             .collect::<Vec<_>>();
 
         let mut counter = HashMap::<u32, u32>::new();
+        let mut tie_break: u32 = 0;
 
-        let mut strength = 0;
-        for key in &cards {
+        let mut strength: u32 = 0;
+        for (i, key) in cards.iter().enumerate() {
             let count = counter.entry(*key).or_insert(0);
+
+            // first card adds value / 15, second over 225, etc...
+            // This should act as a tie breaker for the strength
+            // base 15 because we have less than 15 elements
+
+            tie_break += *key * 15u32.pow((5 - i) as u32);
 
             *count += 1;
             match count {
+                // pair gives 10 point
                 2 => strength += 10,
+                // 3 of a kind gives 100
                 3 => strength += 90,
+                // 4 of a kind gives 1000
                 4 => strength += 900,
+                // 5 of a kind gives 10000
                 5 => strength += 9000,
                 _ => {}
             }
         }
 
-        // 12345=>0
-        // 11234=>10
-        // 11223=>20
-        // 11123=>100
-        // 11122=>110
-        // 11112=>1000
-        // 11111=>10000
-
         Self {
             cards,
             strength,
+            tie_break,
             bid,
         }
     }
@@ -92,12 +124,24 @@ fn parse_line(line: &str) -> IResult<&str, Hand> {
     .parse(line)
 }
 
+fn parse_lines(input: &str) -> IResult<&str, Vec<Hand>> {
+    separated_list1(line_ending, parse_line)(input)
+}
+
 #[tracing::instrument]
 pub fn process(
     input: &str,
 ) -> miette::Result<u32, AocError> {
-    parse_line(input).unwrap();
-    Ok(1)
+    let (_, mut hands) = parse_lines(input).unwrap();
+
+    hands.sort();
+    let sum = hands
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) as u32 * h.bid)
+        .sum();
+
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -106,12 +150,12 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("32T3K 765", Hand{cards:vec![3,2,10,3,13], bid: 765, strength:10})]
-    #[case("33T3K 765", Hand{cards:vec![3,3,10,3,13], bid: 765, strength:100})]
-    #[case("33T3T 765", Hand{cards:vec![3,3,10,3,10], bid: 765, strength:110})]
-    #[case("3KT3T 765", Hand{cards:vec![3,13,10,3,10], bid: 765, strength:20})]
-    #[case("3333T 765", Hand{cards:vec![3,3,3,3,10], bid: 765, strength:1000})]
-    #[case("33333 765", Hand{cards:vec![3,3,3,3,3], bid: 765, strength:10000})]
+    #[case("32T3K 765", Hand{cards:vec![3,2,10,3,13], bid: 765, strength: 10, tie_break: 2413995})]
+    #[case("33T3K 765", Hand{cards:vec![3,3,10,3,13], bid: 765, strength: 100, tie_break: 2464620})]
+    #[case("33T3T 765", Hand{cards:vec![3,3,10,3,10], bid: 765, strength: 110, tie_break: 2464575})]
+    #[case("3KT3T 765", Hand{cards:vec![3,13,10,3,10], bid: 765, strength: 20, tie_break: 2970825})]
+    #[case("3333T 765", Hand{cards:vec![3,3,3,3,10], bid: 765, strength: 1000, tie_break: 2440950 })]
+    #[case("33333 765", Hand{cards:vec![3,3,3,3,3], bid: 765, strength: 10000, tie_break: 2440845 })]
     fn test_parse_line(
         #[case] line: &str,
         #[case] expected: Hand,
