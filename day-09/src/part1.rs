bdfe0520c72c78953b80c0b052d26a6be9d78eea
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-12 23:03:28 +0200

    Add part1 day9

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -1,10 +1,55 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+use nom::{
+    character::complete::{self, line_ending, space1},
+    multi::separated_list1,
+    IResult,
+};
+
 use crate::custom_error::AocError;
+
+fn parse_input(
+    input: &str,
+) -> IResult<&str, Vec<Vec<i32>>> {
+    separated_list1(
+        line_ending,
+        separated_list1(space1, complete::i32),
+    )(input)
+}
+
+fn extrapolate(seq: &[i32]) -> i32 {
+    dbg!(&seq);
+    let mut all_zeros = true;
+    let next_seq: Vec<i32> = seq
+        .iter()
+        .tuple_windows()
+        .map(|(t1, t2)| *t2 - *t1)
+        .inspect(|c| {
+            if *c != 0 {
+                all_zeros = false;
+            }
+        })
+        .collect();
+
+    let last_val = *seq.last().unwrap_or(&0);
+
+    if all_zeros {
+        last_val
+    } else {
+        last_val + extrapolate(&next_seq)
+    }
+}
 
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    input: &str,
+) -> miette::Result<i32, AocError> {
+    let (_, lines) = parse_input(input).unwrap();
+
+    let sum: i32 =
+        lines.iter().map(|l| extrapolate(l)).sum();
+
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -13,9 +58,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(114, process(input)?);
         Ok(())
     }
 }
